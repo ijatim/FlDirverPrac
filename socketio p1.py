0e93a63478881c901ba612da765f23ee3dac7dfc
ijatim <hamed.k_99@yahoo.com>
2020-05-13 06:33:44 -0700

    test1

--- socketio p1.py
+++ socketio p1.py
@@ -20,5 +20,6 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('revieved my event: ' + str(json))
     sio.emit('my response', json, callback=receive_massage)
 
+
 if __name__ == '__main__':
     sio.run(app, debug=True)
